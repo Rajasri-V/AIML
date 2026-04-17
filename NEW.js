89d5a90f2dc933099747740d184e5ceb7fbe0c3c
Rajasri-V <raji090921@gmail.com>
2026-04-17 10:59:01 +0530

    Add files via upload

---
+++ NEW.js
@@ -0,0 +1,95 @@
+// // // //  const names=require('./module.js')
+// // const os=require('os')
+// //const fs=require('fs')
+// // // function name1(){
+
+// // fs.writeFile('./data.txt',"new content added",(err)=>{
+// //     if(err){
+// //         console.log("error occured")
+// //         return 
+// //     }
+// //     console.log("file created")
+// // })
+
+// // fs.readFile('./data.txt','utf8',(err,data)=>{
+// //     if(err){
+// //         console.log(err)
+// //     }
+// //     console.log("the data is:",data)
+// // })
+
+
+
+
+// // fs.appendFile('./message.txt', 'data to append', (err) => {
+// //   if (err) throw err;
+// //   console.log('The "data to append" was appended to file!');
+// // });
+
+
+// // fs.unlink('./data.txt', (err) => {
+// //   if (err) throw err;
+// //   console.log('path/file.txt was deleted');
+// // });
+
+
+// // fs.chmod('message.txt', 0o775, (err) => {
+// //   if (err) throw err;
+// //   console.log('The permissions for file "my_file.txt" have been changed!');
+// // });
+
+
+
+
+// // //     console.log("node.js Started")
+// // // }
+// // // name1()
+// // // names.key1()
+// // // names.key2()
+// // // console.log(os.freemem())
+// // // console.log(os.platform())
+// // // console.log(os.arch())
+// console.log(os.cpus())
+// // // console.log(os.hostname())
+// // // console.log(os.release())
+// // // console.log(os.type())
+// // // console.log(os.uptime())
+// // // console.log(os.version())
+// // // console.log(os.machine())
+// // console.log(os.devNull)
+// // console.log(os.endianness)
+// // console.log(os.homedir)
+
+
+
+
+
+
+// // console.log("first")
+// // fs.writeFile('./data.txt',"new content added",(err)=>{
+// //      if(err){
+// //          console.log("error occured")
+// //          return 
+// //      }
+// //      console.log("file created")
+// //  })
+// // console.log("third")
+
+// // console.log("first")
+// // setTimeout(() =>{
+// //     console.log("finshied")
+// // },3000)
+// // console.log("third")
+
+//  const server=http.createServer((req,res) => {
+//     res.write("server side js")
+//     res.end()
+// })
+// server.listen(3000,()=>
+// {
+//     console.log("server is running")
+// // }
+
+
+//console.log("express.js stated  nodemon")
+
